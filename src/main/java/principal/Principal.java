47de799d95e70f2a2ea7d0e94feb8994e395fb9c
Luan Galatti <60677273+LuanGalatti@users.noreply.github.com>
2025-04-15 11:13:52 -0300

    Update: Arrumado tabulação em Principal

--- src/main/java/principal/Principal.java
+++ src/main/java/principal/Principal.java
@@ -13,19 +13,15 @@ public class Principal {
         while (!opcao.equals("9")) {
             opcao = JOptionPane.showInputDialog("1 - Leitura Triangulo \n2 - Area Triangulo \n3 - Leitura Retangulo \n4 - Area Retangulo \n9 - Sair ");
             if (opcao.equals("1")) {
-                triangulo.setBase(Double.parseDouble(
-                        JOptionPane.showInputDialog("Digite a Base do Triangulo")));
-                triangulo.setAltura(Double.parseDouble(
-                        JOptionPane.showInputDialog("Digite a Altura do Triangulo")));
+                triangulo.setBase(Double.parseDouble(JOptionPane.showInputDialog("Digite a Base do Triangulo")));
+                triangulo.setAltura(Double.parseDouble(JOptionPane.showInputDialog("Digite a Altura do Triangulo")));
             } else {
                 if (opcao.equals("2")) {
                     JOptionPane.showMessageDialog(null, "Area Triangulo: " + triangulo.getArea());
                 } else {
                     if (opcao.equals("3")) {
-                        retangulo.setBase(Double.parseDouble(JOptionPane.showInputDialog(
-                                "Digite a Base do Retangulo")));
-                        retangulo.setAltura(Double.parseDouble(JOptionPane.showInputDialog(
-                                "Digite a Altura do Retangulo")));
+                        retangulo.setBase(Double.parseDouble(JOptionPane.showInputDialog("Digite a Base do Retangulo")));
+                        retangulo.setAltura(Double.parseDouble(JOptionPane.showInputDialog("Digite a Altura do Retangulo")));
                     } else {
                         if (opcao.equals("4")) {
                             JOptionPane.showMessageDialog(null, "Area Retangulo: " + retangulo.getArea());
